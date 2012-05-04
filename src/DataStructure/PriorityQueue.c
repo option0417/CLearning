dc329d495b86bf322e9f8e9d4162eabaf291ae55
option0417 <option0417@gmail.com>
2012-05-04 16:06:05 +0800

    Updated

---
+++ src/DataStructure/PriorityQueue.c
@@ -0,0 +1,40 @@
+/*
+ * PriorityQueue.c
+ *
+ *  Created on: May 2, 2012
+ *      Author: option0417
+ */
+#include "PriorityQueue.h"
+
+int* createPriorityQueue(int size) {
+	int *node = (int*)malloc(sizeof(int) * size);
+	int cnt = 0;
+	while(cnt < size)	node[cnt] = -1;
+	return node;
+}
+
+void push(int* root, int size, int val) {
+	int cnt = -1;
+	int flag = 0;
+	while(++cnt < size) {
+		if (root[cnt] == -1) {
+			root[cnt] = val;
+			flag = 1;
+		}
+	}
+
+	if (flag) {
+
+	}
+
+	if (!flag) printf("Queue Full\n");
+}
+
+int pop(int* root, int size) {
+	return 0;
+}
+
+void destroy(int* root) {
+	free(root);
+}
+
---
+++ src/DataStructure/PriorityQueue.h
@@ -0,0 +1,18 @@
+/*
+ * PriorityQueue.h
+ *
+ *  Created on: May 2, 2012
+ *      Author: option0417
+ */
+
+#ifndef PRIORITYQUEUE_H_
+#define PRIORITYQUEUE_H_
+
+int* createPriorityQueue(int);
+void push(int*, int, int);
+int pop(int*, int);
+void destroy(int*);
+//int size(int*);
+
+
+#endif /* PRIORITYQUEUE_H_ */
