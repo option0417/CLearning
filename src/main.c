f1d6c5fc635bd56ebac61a3b8364897af4d2066e
option0417 <option0417@gmail.com>
2013-03-28 00:44:52 +0800

    Updated

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #include "Problems/PLATEAU.h"
 #include "Problems/GT_COUNT.h"
 #include "Problems/EQ_COUNT.h"
+#include "Problems/MINDIST.h"
 
 
 int main(int argc, char* argv[]) {
@@ -25,11 +26,13 @@ int main(int argc, char* argv[]) {
 	//int plateau[12] = {1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4};
 	//showMaxLengthPlateau(plateau, 12);
 
-	int f[5] = {1, 3, 5, 7, 9};
-	int g[5] = {2, 3, 4, 7, 8};
+	int f[5] = {10, 30, 50, 70, 90};
+	int g[5] = {13, 16, 20};
 	//printf("GT Count : %d", getGTCount(f, 5, g, 5));
 
-	printf("EQ Count : %d", getEQCount(f, 5, g, 5));
+	//printf("EQ Count : %d", getEQCount(f, 5, g, 5));
+
+	printf("MinDist : %d", getMinDist(f, 5, g, 3));
 
 	return EXIT_SUCCESS;
 }
