3d41c513f5fbb6372e2deb197e4dbd9a1c758591
option0417 <option0417@gmail.com>
2013-03-27 22:55:42 +0800

    Add MINDIST problem

--- src/main.c
+++ src/main.c
@@ -11,12 +11,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Algorithms/Demo.h"
-#include "Problems/Knapsack.h"
 #include "Others/FileReader.h"
+#include "Problems/Knapsack.h"
+#include "Problems/PLATEAU.h"
+#include "Problems/GT_COUNT.h"
+#include "Problems/EQ_COUNT.h"
 
 
 int main(int argc, char* argv[]) {
 	//doDemo();
-	doKnapsack();
+	//doKnapsack();
+
+	//int plateau[12] = {1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4};
+	//showMaxLengthPlateau(plateau, 12);
+
+	int f[5] = {1, 3, 5, 7, 9};
+	int g[5] = {2, 3, 4, 7, 8};
+	//printf("GT Count : %d", getGTCount(f, 5, g, 5));
+
+	printf("EQ Count : %d", getEQCount(f, 5, g, 5));
+
 	return EXIT_SUCCESS;
 }
