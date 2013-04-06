c27b85b331ff4e159bba7971414cd813527a64ce
option0417 <option0417@gmail.com>
2013-04-06 18:13:52 +0800

    Add I_SIEVE problem

---
+++ src/Problems/I_SIEVE.h
@@ -0,0 +1,13 @@
+/*
+ * I_SIEVE.h
+ *
+ *  Created on: Apr 5, 2013
+ *      Author: option0417
+ */
+
+#ifndef I_SIEVE_H_
+#define I_SIEVE_H_
+
+int showPrimeWithISieve(int );
+
+#endif /* I_SIEVE_H_ */
