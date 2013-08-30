9c22d16e9de0130bc94b515b1499fa0ca9eedd9f
1010481 <1010481@1010481nb01.iead.local>
2013-08-30 11:03:17 +0800

    Add Quadratic_equation

---
+++ src/Others/quadratic_equation.h
@@ -0,0 +1,17 @@
+/*
+ * quadratic_equation.h
+ *
+ *  Created on: 2013/8/30
+ *      Author: 1010481
+ */
+
+#ifndef QUADRATIC_EQUATION_H_
+#define QUADRATIC_EQUATION_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void  quadratic_equation(int , int , int );
+
+#endif /* QUADRATIC_EQUATION_H_ */
