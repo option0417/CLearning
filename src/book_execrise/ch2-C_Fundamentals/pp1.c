219fa393f01c9019df72a9fcbe7820a7de9606c0
WeiDe Su <weide_su@mitake.com.tw>
2018-08-01 13:17:57 +0800

    Add printStar2 for p2

--- src/book_execrise/ch2-C_Fundamentals/pp1.c
+++ src/book_execrise/ch2-C_Fundamentals/pp1.c
@@ -18,8 +18,35 @@ void printStar() {
   printf("  *\n");
 }
 
+void printStar2() {
+  int maxRound      = 6;
+  int initSpaceLength = 8;
+
+  while (maxRound-- > 0) {
+    int spaceLength = initSpaceLength;
+    
+    if (initSpaceLength == 5) {
+      printf("*");
+      spaceLength -= 1;
+    } else if (initSpaceLength == 4) {
+      printf(" *");
+      spaceLength -= 2;
+    }
+   
+
+    while (spaceLength-- > 0) {
+      printf(" ");
+    }
+    printf("*\n");
+
+    initSpaceLength--;
+  }
+}
 
 int main(void) {
   printStar();
 
+  printf("----------\n");
+
+  printStar2();
 }
