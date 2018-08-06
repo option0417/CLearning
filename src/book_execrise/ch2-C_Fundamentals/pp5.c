0bea1a9a4c7f4438935fa1ba7fe34347203838f1
W.D Su <option0417@gmail.com>
2018-08-07 01:07:41 +0800

    Complete pp5

--- src/book_execrise/ch2-C_Fundamentals/pp5.c
+++ src/book_execrise/ch2-C_Fundamentals/pp5.c
@@ -10,13 +10,16 @@
 #include <stdio.h>
 
 
-int expo(int val, int exp) {
-  while (exp-- > 0) val *= val;
+int calExp(int val, int expTimes) {
+  int exp = val;
+  while (expTimes-- > 1) val *= exp;
   return val;
 }
 
 /*
  * 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6
+ * Ans: 0 (x = 1)
+ * Ans: 258 (x = 2)
  */
 int main(void) {
   int x = 0;
@@ -24,7 +27,12 @@ int main(void) {
   printf("Enter a value: ");
   scanf("%d", &x);
 
+  printf("%d\n", calExp(x, 5));
+  printf("%d\n", calExp(x, 4));
+  printf("%d\n", calExp(x, 3));
+  printf("%d\n", calExp(x, 2));
   
+  printf("Result: %d\n", 3 * calExp(x, 5) + 2 * calExp(x, 4) - 5 * calExp(x, 3) - calExp(x, 2) + 7 * x - 6);  
 
   return 0;
 }
